786152143cd5a38cb1782895985d466ac9c17839
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2016-08-23 15:55:38 +0200

    tmux: Add initial config

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ ln -s "$DOTFILES/.zsh/zshrc"           "$HOME/.zshrc"
 # VIM
 ln -s "$DOTFILES/vim"                  "$HOME/.vim"
 ln -s "$HOME/.vim/vimrc"               "$HOME/.vimrc"
+
+# Tmux
+ln -s "$DOTFILES/tmux"                 "$HOME/.tmux"
+ln -s "$HOME/.tmux/tmux.conf"          "$HOME/.tmux.conf"
