fd53dc747b7910fd6e7e6d24f8e0bd9e5561a083
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2016-09-06 12:47:01 +0200

    git: Add global git config

--- setup.sh
+++ setup.sh
@@ -11,3 +11,6 @@ ln -s "$HOME/.vim/vimrc"               "$HOME/.vimrc"
 # Tmux
 ln -s "$DOTFILES/tmux"                 "$HOME/.tmux"
 ln -s "$HOME/.tmux/tmux.conf"          "$HOME/.tmux.conf"
+
+# GIT
+ln -s "$DOTFILES/git/gitignore_global" "$HOME/.gitignore_global"
