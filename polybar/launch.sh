063af5ef3e7377b3430c39f8dca7401d3450b442
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2018-03-07 13:58:23 +0100

    polybar: Improve launch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-if ! pgrep -f "polybar .* primary"; then
+if ! pgrep -f "polybar .* primary" >/dev/null; then
+    echo "Starting primary polybar..."
     polybar --reload primary &
 fi
 
-if ! pgrep -f "polybar .* secondary"; then
-    polybar --reload secondary &
+if ! xrandr -q | grep -q "VGA-0 connected"; then
+    echo "Killing secondary polybar..."
+    pkill -f "polybar .* secondary"
+else
+    if ! pgrep -f "polybar .* secondary" >/dev/null; then
+        echo "Starting secondary polybar..."
+        polybar --reload secondary &
+    fi
 fi
