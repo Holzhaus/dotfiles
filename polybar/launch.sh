ff85c717309d7c8cba56513b668de61b69b0999d
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2018-04-10 10:54:48 +0200

    polybar: Implement a fancier launch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,15 +1,48 @@
 #!/bin/sh
-if ! pgrep -f "polybar .* primary" >/dev/null; then
-    echo "Starting primary polybar..."
-    polybar --reload primary &
+# Check which bars should be launched
+PROFILE="$1"
+
+if [ -z "$PROFILE" ]
+then
+    PROFILE="docked"
+    printf 'Using default polybar profile "%s"\n' "$PROFILE"
 fi
 
-if ! xrandr -q | grep -q "VGA-0 connected"; then
-    echo "Killing secondary polybar..."
-    pkill -f "polybar .* secondary"
-else
-    if ! pgrep -f "polybar .* secondary" >/dev/null; then
-        echo "Starting secondary polybar..."
-        polybar --reload secondary &
-    fi
-fi
+case "$PROFILE" in
+    "none")
+        POLYBAR_NAMES=""
+        ;;
+    "docked")
+        POLYBAR_NAMES="primary secondary"
+        ;;
+    "mobile")
+        POLYBAR_NAMES="primary"
+        ;;
+    "")
+        printf 'Using default polybar profile "docked"\n'
+        POLYBAR_NAMES="primary secondary"
+        ;;
+    *)
+        printf 'Usage: %s {none|docked|mobile}\n' "$0"
+        exit 1
+esac
+
+# Kill old instances and wait for them to terminate
+pkill -TERM -e polybar | grep -ioP '(pid \K\d+)' | while read -r PID
+do
+    while ps --pid "$PID" > /dev/null
+    do
+        printf 'Waiting for polybar with pid %d to terminate...\n' "$PID"
+        sleep 0.1
+    done
+    printf 'Polybar with pid %d terminated\n' "$PID"
+done
+
+# Early exit if no polybar should be launched
+[ -z "$POLYBAR_NAMES" ] && exit 0
+
+for POLYBAR_NAME in $POLYBAR_NAMES
+do
+    printf 'Launching polybar "%s"\n' "$POLYBAR_NAME"
+    polybar --reload "$POLYBAR_NAME" &
+done
