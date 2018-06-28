f34cce6aa8a5e59a07f4ed0de4ab4ed4ad9844d0
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2018-06-28 17:13:32 +0200

    qutebrowser: Silence linter errors in config.py

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -3,6 +3,11 @@ import subprocess
 import os
 import yaml
 
+# Silence linter errors
+# pylint: disable=C0111
+c = c  # noqa: F821 pylint: disable=E0602,C0103
+config = config  # noqa: F821 pylint: disable=E0602,C0103
+
 
 def read_xresources(prefix):
     props = {}
