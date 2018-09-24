27a85de4843218f80f796b0088affa703bfbff57
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2018-09-24 12:45:24 +0200

    bin: Move i3lock-multimonitor from i3/ to bin/

--- xinitrc.d/11-screenlocker.sh
+++ xinitrc.d/11-screenlocker.sh
@@ -2,4 +2,4 @@
 # Screensaver / Locker
 xset s 180 180
 xset dpms 300 600 900
-xss-lock -l -- "$HOME/.i3/i3lock-multimonitor.sh" "$HOME/.i3/lockscreen.jpg" -c 000000 -f -t &
+xss-lock -l -- "$HOME/.local/bin/i3lock-multimonitor" "$HOME/.i3/lockscreen.jpg" -c 000000 -f -t &
