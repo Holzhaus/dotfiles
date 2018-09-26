707c1290006d721ec0702f598df97a7421600eb2
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2018-09-26 18:49:10 +0200

    xinitrc: Reduce screensaver/screenlocker timeout

--- xinitrc.d/11-screenlocker.sh
+++ xinitrc.d/11-screenlocker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Screensaver / Locker
 xset s 180 180
-xset dpms 300 600 900
+xset dpms 180 180 180
 xss-lock -l -- "$HOME/.local/bin/i3lock-multimonitor" "$HOME/.i3/lockscreen.jpg" -c 000000 -f -t &
