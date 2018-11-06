e4448ce08a911747c096f1ba5de1f29edd94efc2
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2018-11-06 17:36:00 +0100

    Xresources: Allow host-specific configs

--- xinitrc.d/01-xresources.sh
+++ xinitrc.d/01-xresources.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 # Load Xresources
 : "${HOME?Need to set HOME}"
-xrdb -merge "$HOME/.Xresources"
+
+[ -z "$HOSTNAME" ] && HOSTNAME="$(hostname)"
+
+xrdb -load "$XRESOURCES"
+
+XRESOURCES_HOSTCONFIG="$HOME/.Xresources.d/$HOSTNAME.xrdb"
+if [ -r "$XRESOURCES_HOSTCONFIG" ]
+then
+    xrdb -merge "$XRESOURCES_HOSTCONFIG"
+fi
